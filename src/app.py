f4a1a8cfd7b1ee122f0ef58b946a846425f85174
fred <you@example.com>
2020-10-11 17:01:06 -0400

    Setup basic heroku depoyment.

--- src/app.py
+++ src/app.py
@@ -1,1 +1,15 @@
-import flask
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# home route
+@app.route("/")
+def welcome():
+    """List available api routes."""
+    return (
+        f'Welcome!!!'
+    )
+
+# run app
+if __name__ == '__main__':
+    app.run(debug=True)
