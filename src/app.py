149b45c00553b6587ae23af87f75f0515244f65b
fred <you@example.com>
2020-10-11 17:14:24 -0400

    Updated port.

--- src/app.py
+++ src/app.py
@@ -12,4 +12,5 @@ def welcome():
 
 # run app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) # this sets the port to 5000 locally, and ${PORT} environment variable on Heroku
+    app.run(host='0.0.0.0', port=port)
