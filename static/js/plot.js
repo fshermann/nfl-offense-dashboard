5a0a9e2678f84ce78473e89553258ec1ac3d285a
fred <you@example.com>
2020-10-14 15:01:32 -0400

    Updated layout and visualizations.

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,69 +1,46 @@
-var passing_layout = {
-    title: {
-        text: 'NFL Passing Yards Vs Passing Touchdowns',
-        font: {
-            family: 'Helvetica Neue',
-            size: 36
+function makeLayout( xAxis, yAxis) {
+
+    // update headers
+    document.getElementById("plot-title").innerText = xAxis + ' And ' + yAxis;
+    document.getElementById("table-title").innerText = xAxis + ' And ' + yAxis;
+
+    var layout = {
+        margin: {
+            l: 60,
+            r: 0,
+            b: 50,
+            t: 0
         },
-        xref: 'paper',
-        x: 0.05,
-    },
-    xaxis: {
-        title: {
-            text: 'Passing Yards',
-            font: {
-                family: 'Helvetica Neue',
-                size: 24,
-                color: '#7f7f7f'
-            }
+        xaxis: {
+            title: {
+                text: xAxis,
+                    font: {
+                        family: 'Sans Serif',
+                        size: 24,
+                            color: '#7f7f7f'
+                }
+            },
         },
-    },
-    yaxis: {
-        title: {
-            text: 'Passing Touchdowns',
-            font: {
-                family: 'Helvetica Neue',
-                size: 24,
-                color: '#7f7f7f'
+        yaxis: {
+            title: {
+                text: yAxis,
+                    font: {
+                        family: 'Sans Serif',
+                        size: 24,
+                            color: '#7f7f7f'
+                }
             }
         }
     }
+
+    return layout
 }
 
-var rushing_layout = {
-    title: {
-        text: 'NFL Rushing Yards Vs Rushing Touchdowns',
-        font: {
-            family: 'Helvetica Neue',
-            size: 36
-        },
-        xref: 'paper',
-        x: 0.05,
-    },
-    xaxis: {
-        title: {
-            text: 'Rushing Yards',
-            font: {
-                family: 'Helvetica Neue',
-                size: 24,
-                color: '#7f7f7f'
-            }
-        },
-    },
-    yaxis: {
-        title: {
-            text: 'Rushing Touchdowns',
-            font: {
-                family: 'Helvetica Neue',
-                size: 24,
-                color: '#7f7f7f'
-            }
-        }
-    }
-}
 d3.json('https://hermann-nfl-offense-dashboard.herokuapp.com/passing-yards-tds').then((d) =>{
     // create scatter plot
-    var passing_plot = document.getElementById('plot');
+
+    var layout = makeLayout("Passing Yards", "Passing Touchdowns")
+    var plot = document.getElementById('plot');
     var trace = {
         x: Object.values(d.passing_yards),
         y: Object.values(d.passing_tds),
@@ -72,20 +49,5 @@ d3.json('https://hermann-nfl-offense-dashboard.herokuapp.com/passing-yards-tds')
         type: 'scatter'
     }
     var data = [trace]
-    Plotly.newPlot(passing_plot, data, passing_layout, { responsive: true })
+    Plotly.newPlot(plot, data, layout, { responsive: true, displayModeBar: false })
 })
-
-d3.json('https://hermann-nfl-offense-dashboard.herokuapp.com/rushing-yards-tds').then((d) => {
-    // create scatter plot
-    var rushing_plot = document.getElementById('plot2');
-    var trace = {
-        x: Object.values(d.rushing_yards),
-        y: Object.values(d.rushing_tds),
-        text: Object.values(d.name),
-        mode: 'markers',
-        type: 'scatter'
-    }
-    var data = [trace]
-    Plotly.newPlot(rushing_plot, data, rushing_layout, { responsive: true })
-})
-
